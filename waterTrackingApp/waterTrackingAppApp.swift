30650016d188449d317834032bd483b1149f565a
earthaSTA <danah.8610@gmail.com>
2024-10-20 14:21:58 +0300

    Initial Commit

---
+++ waterTrackingApp/waterTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterTrackingAppApp.swift
+//  waterTrackingApp
+//
+//  Created by Danah Alsultan on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct waterTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
